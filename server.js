bd622c3f64ab3aa16af57f32c6c035f806954116
Major <navikbipin@gmail.com>
2021-02-11 17:56:19 +0530

    using Chalk package

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 require("./Database/db");
+const chalk = require('chalk')
 const http = require('http');
 
 const { env } = require('process');
 const app = require('./index');
 const port = process.env.PORT || 3200;
 const server = http.createServer(app);
-server.listen(port, ()=> {
-    console.log(`Server started at port no ${port}`)
-} )
+// server.listen(port, ()=> {
+//     console.log(chalk.bgYellowBright.red.bold(`Server started at port no ${port}`))
+// } )
+app.listen(port, () => {
+    console.log(
+      `${chalk.cyan('✓ ')}` + `${chalk.bgWhite.red.bold(
+        ` Listening on port ${port}. Visit http://localhost:${port}/ in your browser.`
+      )}`
+    );
+  });
